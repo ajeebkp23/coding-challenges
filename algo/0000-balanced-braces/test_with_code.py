5e15b793274bffa842e866f80ccc9a3d222c90fd
Ajeeb K P <ajeebkp23@gmail.com>
2020-12-31 23:17:07 +0530

    Adding balanced-braces problem - passed all tests

--- algo/0000-balanced-braces/test_with_code.py
+++ algo/0000-balanced-braces/test_with_code.py
@@ -12,15 +12,26 @@
 
 
 def solve(data):
-    brackets = [
-        ('{','}'),
-        ('(',')'),
-        ('[',']'),
-    ]
-    for bracket1, bracket2 in brackets:
-        if data.count(bracket1) != data.count(bracket2):
-            return False
-    return True
+    open_brackets = ['{','(','[']
+    close_brackets = ['}',')',']']
+    brackets = dict(zip(open_brackets,close_brackets))
+    is_balanced = True
+    bracket_in_data = []
+    for i in data:
+        print(i)
+        if i in open_brackets:
+            bracket_in_data.append(i)
+        elif i in close_brackets:
+            if brackets[bracket_in_data[-1]] == i:
+                bracket_in_data.pop(-1)
+            else:
+                is_balanced = False
+                break
+        else:
+            pass
+    if len(bracket_in_data) != 0:
+        is_balanced = False
+    return is_balanced
 
 def test_datas():
     inputs_ouputs = [
@@ -32,7 +43,8 @@ def test_datas():
         ("if(a==b x = y;" , False),
         ("if(x<10}(b++;}else{b+=10;}" , False),
     ]
-
+    print(inputs_ouputs)
     for inp, outp in inputs_ouputs:
         print('Input and Output are: ',inp,outp)
         assert solve(inp) == outp
+        print('Input and Output are [Balanced]: ',inp,outp)
